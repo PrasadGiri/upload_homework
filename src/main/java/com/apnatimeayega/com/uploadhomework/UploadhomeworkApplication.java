086e264fcf543b56d33200e57cdab0ea47a45d14
prasad <prasad.giri@msm.digital>
2020-07-16 21:13:57 +0530

    [PG] first commit

---
+++ src/main/java/com/apnatimeayega/com/uploadhomework/UploadhomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.apnatimeayega.com.uploadhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadhomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadhomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apnatimeayega/com/uploadhomework/UploadhomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apnatimeayega.com.uploadhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadhomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
